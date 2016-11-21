07e7c64779d341f85ca4a6fcf6046127baad7985
Your Name <you@example.com>
2016-11-21 16:32:32 +0500

    initial state machine

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  state_machine :initial => :unknown, :attribute => :state_value do
+    event :apply_to_store do
+      transition [:unknown,:repair,:site] => :store
+    end
+    event :apply_to_site do
+      transition :store => :site
+    end
+    event :apply_to_recycle do
+      transition [:store,:repair] => :recycle
+    end
+    event :apply_to_repair do
+      transition :store => :repair
+    end
+  end
 end
