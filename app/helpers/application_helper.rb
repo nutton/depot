9489fa5c4347288435cb85d3c97d8162b9ca289a
Your Name <you@example.com>
2016-11-23 15:36:39 +0500

    gui tuning: part n of n-k with k -> inf

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def title_content
     content_for(:title) || ""
   end
+  def lwi(path,text,icon)
+  	link_to path do 
+  		t = content_tag :i, nil, :class => icon
+  		t+' '+text
+  	end
+  end
+
+  def menu_helper
+
+  	if user_signed_in?
+  	end
+  end
+
 end
