5c49a406a9d78cb078b24e19689ce104d3a18f38
homework <home@example.com>
2016-11-27 17:42:07 +0500

    dunno

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121064112) do
+ActiveRecord::Schema.define(version: 20161121093517) do
 
   create_table "configurations", force: :cascade do |t|
     t.integer  "vendor_id"
